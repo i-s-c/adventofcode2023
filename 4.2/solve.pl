46cab91a5ca8edc8573960895f4cd9048efb15d2
Ian Castle <iancastle@Ians-iMac.local>
2023-12-04 11:07:00 +0000

    Tidy it up because why not.

--- 4.2/solve.pl
+++ 4.2/solve.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
-my $sum = 0;
+my $deck;
 
-my %deck;
-my $d = \%deck;
+# 1. Parse the input and read it into the deck of cards. Work out the number of wins
+# Each card has a number of wins and the count of instances of the card
 while ( my $line = <STDIN> ) {
 	chomp($line);
 
-	$line =~ s/  / /g;
+	$line =~ s/  / /g; # Perl is very sensitive to whitespace. Just get rid of multuples
 	my ( $name, $rest ) = split(/:/,$line);
 
 	my $id = $name;
-	$id =~ tr/0-9//cd;
+	$id =~ tr/0-9//cd; # Each card has a number, this identifies the card
 
-	$rest =~ s/^ //;
-	
+	$rest =~ s/^ //; # Get rid of leading white space.
 
 	my ( $winnerstring, $numberstring ) = split( /\|/, $rest );
 
@@ -26,7 +26,9 @@ while ( my $line = <STDIN> ) {
 	my @winners = split( / /, $winnerstring );
 	my @numbers = split( / /, $numberstring );
 
-	# Let a hash take the strain
+	# We now have a list of winning numbers and the playing numbers.
+
+	# Let a hash take the strain: So we don't have to loop round the list of winners all the time. Just makes it easier to read later.
 
 	my %win;
 
@@ -35,33 +37,35 @@ while ( my $line = <STDIN> ) {
 	}
 
 	my $wins = 0;
+	# See how many of the numbers are wins
 	foreach my $n ( @numbers ) {
 		if( defined($win{$n}) ) {
 			$wins++;
 		}
 	}
 
-	$d->{$id}->{count}=1;
-	$d->{$id}->{wins}=$wins;
+	# Add that information to the deck
+	$deck->{$id}->{count}=1;
+	$deck->{$id}->{wins}=$wins;
 
 	
 }
 
-foreach my $i ( sort { $a <=> $b } keys %$d ) {
+# Now duplicate the cards as per part 2 instructions. Go through the deck, duplicate the cards. Theoretically, we might need boundary conditions, but the data is empirically OK.
+# Keep a running total after we've finished each card.
 
-	print "Processing card $i which has a count of $d->{$i}->{count} and $d->{$i}->{wins} wins\n";
-	for ( my $c = 0; $c < $d->{$i}->{count}; $c++ ) {
-		for ( my $j = $i + 1; $j <= $i + $d->{$i}->{wins}; $j++ ) {
-			$d->{$j}->{count} += 1;
-			print "\tDuplicating card $j so there are now $d->{$j}->{count} of them with $d->{$j}->{wins}\n";
+my $sum = 0;
+foreach my $i ( sort { $a <=> $b } keys %$deck ) {
+
+	print "Processing card $i which has a count of $deck->{$i}->{count} and $deck->{$i}->{wins} wins\n";
+	for ( my $c = 0; $c < $deck->{$i}->{count}; $c++ ) {
+		for ( my $j = $i + 1; $j <= $i + $deck->{$i}->{wins}; $j++ ) {
+			$deck->{$j}->{count} += 1;
+			print "\tDuplicating card $j so there are now $deck->{$j}->{count} of them with $deck->{$j}->{wins}\n";
 		}
 	}
-}
-
-
-foreach my $i ( sort { $a <=> $b } keys %$d ) {
-	print "Card $i has a total of $d->{$i}->{count}\n";
-	$sum += $d->{$i}->{count};
+	
+	$sum += $deck->{$i}->{count};
 }
 
 print "SUM: $sum\n";
