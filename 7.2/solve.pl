9afb1ee46fe3370cab78fd78269154f64efaa516
Ian Castle <iancastle@Ians-iMac.local>
2023-12-07 16:48:47 +0000

    This ought to work - gets the test answer correct. But fails on the big data set. I suspect it is my interpretation of the rules again.

--- 7.2/solve.pl
+++ 7.2/solve.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+$| = 1;
+
 
 
 my $hands;
@@ -15,6 +17,8 @@ my %valuecards;
 foreach my $card ( keys %cardvalues ) {
 	$valuecards{$cardvalues{$card}} = $card;
 }
+
+my %description = ( 5 => "5 different cards", 9 => "ONE Pair", 13 => "TWO Pairs", 38 => "Three of a kind", 42 => "Three of a kind and a pair", 217 => "FOUR of a kind", 1296 => "FIVE of a Kind" );
 
 while ( my $line = <STDIN> ) {
 	chomp($line);
@@ -53,26 +57,63 @@ foreach my $hand ( keys %$hands ) {
 
 	# Does it matter than we are using the jokers more than once?
 	foreach my $card ( @cards ) {
-		if ( $card ne "J" and !defined( $pairs{$card} ) ) {
-			$pairs{$card} = $jokers;
-		}
+		#if ( $card ne "J" and !defined( $pairs{$card} ) ) {
+		#	$pairs{$card} = $jokers;
+		#}
 
 		$pairs{$card} += 1;
 	}
 
+	# Which one shall we apply the jokers to
+
+	if ( $jokers > 0 ) {
+		my @bestpair;
+
+		foreach my $card ( keys %pairs ) {
+			@bestpair = ( @bestpair, sprintf( "%02d.%02d", $pairs{$card}, $cardvalues{$card} ));
+		}
+
+		print "\tbest pairs are: " . join(", ", @bestpair ) . "\n";
+
+		my $bp = ( sort { $b <=> $a } @bestpair )[0];
+
+		print "\tthe best pair is $bp\n";
+
+		my ( $bpp, $bpv ) = split(/\./,$bp );
+
+		$bp = int($bpv);
+		print "\tThe card has a value of $bp\n";
+
+		my $wildcard = $valuecards{$bp};
+
+		if ( $wildcard ne "J" ) {
+			printf("\tthe card is %s, using the %d jokers to make %d of %s\n", $wildcard, $jokers,  $pairs{$wildcard} + $jokers, $wildcard);
+
+			$pairs{$wildcard} += $jokers;
+			delete $pairs{"J"};
+		}
+		else {
+			print "\tDon't need to make a joker a joker!\n";
+		}
+	}
 	
 	my $pairscore = 0;
 	
-
+	my $wildcardhand = "";
 	foreach my $card ( keys %pairs ) {
 		$pairscore += 6 ** ($pairs{$card}-1);
 
 		print "\t\tWe have $pairs{$card} of $card\n";
+
+		for ( my $i = 0; $i < $pairs{$card}; $i++ ) {
+			$wildcardhand .= "$card";
+		}
 	} 
 
-	print "\thand has pairvalue $pairscore\n";
+	print "\thand has pairvalue $pairscore, with wildcards it looks like $wildcardhand instead of $hand\n";
 
 	$hands->{$hand}->{pairvalue} = $pairscore;
+	$hands->{$hand}->{wildcardhand} = $wildcardhand;
 
 	#$handvalue =  $handvalue  + ( $pairscore * ( 14 ** 6 ) );
 	$handvalue = sprintf( "%04d%s", $pairscore, $handvalue );
@@ -92,11 +133,11 @@ foreach my $hand ( keys %$hands ) {
 
 my $rank = 1;
 my $sum = 0;
-printf( "rank\tpairscore\tvalue\thand\ttank\tbid\tscore\n");
+printf( "rank\t%-20s\tpairscore\tvalue\thand\twildcard\trank\tbid\tscore\n", "description");
 foreach my $value (sort { $a <=> $b } keys %values ) {
 	my $hand = $values{$value};
 	my $score = $rank * $hands->{$hand}->{bid};
-	printf( "%d\t%d\t%s\t%s\t%d\t%d\t%d\n", $rank, $hands->{$hand}->{pairvalue}, $value, $hand, $rank, $hands->{$hand}->{bid}, $score );
+	printf( "%d\t%-30s\t%d\t%s\t%s\t%s\t%d\t%d\t%d\n", $rank, $description{$hands->{$hand}->{pairvalue}}, $hands->{$hand}->{pairvalue}, $value, $hand, $hands->{$hand}->{wildcardhand}, $rank, $hands->{$hand}->{bid}, $score );
 	$sum += $score;
 	$rank++;
 }
