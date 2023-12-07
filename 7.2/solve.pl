38ceb786d58c5a5f5077fa72ab9c13e050050482
Ian Castle <iancastle@Ians-iMac.local>
2023-12-07 17:55:39 +0000

    Just tidy things up and add some comments.

--- 7.2/solve.pl
+++ 7.2/solve.pl
@@ -5,8 +5,6 @@ use warnings;
 
 $| = 1;
 
-
-
 my $hands;
 my %values;
 
@@ -20,13 +18,12 @@ foreach my $card ( keys %cardvalues ) {
 
 my %description = ( 5 => "5 different cards", 9 => "ONE Pair", 13 => "TWO Pairs", 38 => "Three of a kind", 42 => "Three of a kind and a pair", 217 => "FOUR of a kind", 1296 => "FIVE of a Kind" );
 
+# Read the hands into memory
+
 while ( my $line = <STDIN> ) {
 	chomp($line);
 	my ( $hand, $bid ) = split( / /, $line );
 
-	#if ( $hand eq "JJJJJ" ) {
-	#	$hand = "AAAAA";
-	#}
 	$hands->{$hand}->{bid} = $bid;
 }
 
@@ -37,14 +34,12 @@ foreach my $hand ( sort keys %$hands ) {
 	print "Hand: $hand\n";
 	my @cards = split(//,$hand);
 
-
 	my $handvalue = "";
 	foreach my $card ( @cards ) {
 		$handvalue .= sprintf( "%02d", $cardvalues{$card} );
 	}
 
-	# Now work out the pair score
-	my %pairs;
+	# Just count the jokers.
 
 	my $jokers = 0;
 
@@ -54,20 +49,19 @@ foreach my $hand ( sort keys %$hands ) {
 		}
 	}
 
-	print "We have $jokers jokers\n";
+	print "\tWe have $jokers jokers\n";
 
-	# Work out the pairs, this time with the jokers
+	# Work out the pairs, now using jokers
+	# Pairs are really the number of times each card occurs in the hand
 
-	# Does it matter than we are using the jokers more than once?
+	my %pairs;
+
 	foreach my $card ( @cards ) {
-		#if ( $card ne "J" and !defined( $pairs{$card} ) ) {
-		#	$pairs{$card} = $jokers;
-		#}
-
 		$pairs{$card} += 1;
 	}
 
-	# Which one shall we apply the jokers to
+	# Which one shall we apply the jokers to?
+	# Need to pick one. So we'll pick the highest scoring card.
 
 	if ( $jokers > 0 ) {
 		my @bestpair;
@@ -78,6 +72,7 @@ foreach my $hand ( sort keys %$hands ) {
 
 		print "\tbest pairs are: " . join(", ", @bestpair ) . "\n";
 
+		# If the "best pair" is a joker, then try again. Only really need to pick the next one.. but why not use a GOTO?
 		for ( my $i = 0; $i < scalar(@bestpair); $i++ ) {
 			my $bp = ( sort { $b <=> $a } @bestpair )[$i];
 
@@ -89,8 +84,6 @@ foreach my $hand ( sort keys %$hands ) {
 			print "\tThe card has a value of $bp\n";
 
 			my $wildcard = $valuecards{$bp};
-
-			
 
 			if ( $wildcard ne "J" ) {
 				printf("\tthe card is %s, using the %d jokers to make %d of %s\n", $wildcard, $jokers,  $pairs{$wildcard} + $jokers, $wildcard);
@@ -107,6 +100,8 @@ foreach my $hand ( sort keys %$hands ) {
 OUT:
 	}
 	
+	# Score the hand so that five of a kind is best, five different cards is the worst.
+
 	my $pairscore = 0;
 	
 	my $wildcardhand = "";
@@ -115,6 +110,8 @@ OUT:
 
 		print "\t\tWe have $pairs{$card} of $card\n";
 
+		# This is just cosmetic for making a nice story - so we get to see what it has used the jokers for. Order isn't important here.
+
 		for ( my $i = 0; $i < $pairs{$card}; $i++ ) {
 			$wildcardhand .= "$card";
 		}
@@ -122,23 +119,30 @@ OUT:
 
 	print "\thand is $description{$pairscore} (a pairvalue of $pairscore), with wildcards it looks like $wildcardhand instead of $hand\n";
 
+	# Stash everything in our complex data structure.
+
 	$hands->{$hand}->{pairvalue} = $pairscore;
 	$hands->{$hand}->{wildcardhand} = $wildcardhand;
 
+	# Get something sortable, pad it with zeros to make it ascii sortable. The type of hand, followed by the value of each card.
 	$handvalue = sprintf( "%04d%s", $pairscore, $handvalue );
 
 	print "\ttotal value is $handvalue\n";
 
 	$hands->{$hand}->{value} = $handvalue;
 
+	# Reverse lookup for later, to be able to get from the value back to the hand. Let's hope hands are unique. If not, we'll need to make it an array, not a scalar.
 	if ( !defined( $values{$handvalue} )) {
 		$values{$handvalue} = $hand;
 	}
 	else {
 		print "Duplicate value for $hand\n";
+		# We'll worry about dealing with this if it ever comes up!
 		exit 0;
 	}
 }
+
+# OK. Everything hand is scored now, wild cards applied, etc. etc. So lets print it out in order, and work out the score.
 
 my $rank = 1;
 my $sum = 0;
