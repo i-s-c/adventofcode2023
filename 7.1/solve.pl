a92fb6b80a6cf23fbfb82e63be40842d78f89783
Ian Castle <iancastle@Ians-iMac.local>
2023-12-07 13:25:33 +0000

    Can't see what is wrong with this!

--- 7.1/solve.pl
+++ 7.1/solve.pl
@@ -10,6 +10,12 @@ my %values;
 
 my %cardvalues = ( "1" => 0, "2" => 1, "3" => 2, "4" => 3, "5" => 4, "6" => 5, "7" => 6, "8" => 7, "9" => 8, "T" => 9, "J" => 10, "Q" => 11, "K" => 12, "A" => 13 );
 
+my %valuecards;
+
+foreach my $card ( keys %cardvalues ) {
+	$valuecards{$cardvalues{$card}} = $card;
+}
+
 while ( my $line = <STDIN> ) {
 	chomp($line);
 	my ( $hand, $bid ) = split( / /, $line );
@@ -33,23 +39,33 @@ foreach my $hand ( keys %$hands ) {
 
 	my $handvalue = 0;
 	my $i = 5;
+	my @orderedhand;
 	foreach my $v ( sort { $b <=> $a } @cv) {
-		$handvalue += $v * ( $i ** 14 );
+		$handvalue += $v * ( 14 ** $i );
+
+		@orderedhand = ( @orderedhand, $valuecards{$v} );
 		$i--;
 	}
 	
 	print "\thand value is $handvalue\n";
+	$hands->{$hand}->{ordervalue} = $handvalue;
+
+	$hands->{$hand}->{ordered} = join("",@orderedhand);
+
+	print "$hand order is " . join("",@orderedhand) . "\n";
 
 	# Now work out the pair score
 	my $pairscore = 0;
 
 	foreach my $card ( keys %pairs ) {
-		$pairscore += 6 ** ( $pairs{$card} );
+		$pairscore += 6 ** ($pairs{$card}-1);
 	} 
 
 	print "\thand has pairvalue $pairscore\n";
 
-	$handvalue =  $handvalue  + ( $pairscore * ( 6 ** 14 ) );
+	$hands->{$hand}->{pairvalue} = $pairscore;
+
+	$handvalue =  $handvalue  + ( $pairscore * ( 14 ** 6 ) );
 
 	print "\ttotal value is $handvalue\n";
 
@@ -68,7 +84,8 @@ my $rank = 1;
 my $sum = 0;
 foreach my $value (sort { $a <=> $b } keys %values ) {
 	my $score = $rank * $hands->{$values{$value}}->{bid};
-	printf( "%d, %d, %s = %d\n", $rank, $value, $values{$value}, $score );
+	my $hand = $values{$value};
+	printf( "%d: %s, %d, %d, %d, %s: %d * %d  = %d\n", $rank, $hands->{$hand}->{ordered}, $hands->{$hand}->{pairvalue}, $hands->{$hand}->{ordervalue}, $value, $hand, $rank, $hands->{$hand}->{bid}, $score );
 	$sum += $score;
 	$rank++;
 }
